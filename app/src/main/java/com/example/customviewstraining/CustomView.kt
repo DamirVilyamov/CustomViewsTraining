f38cf8d84863fe9152f6f27be48f065c979e701d
Damir <vilyamov.damir@gmail.com>
2020-08-23 20:43:22 +0500

    [ADDED] support for custom backgrounds for small buttons

--- app/src/main/java/com/example/customviewstraining/CustomView.kt
+++ app/src/main/java/com/example/customviewstraining/CustomView.kt
@@ -3,75 +3,138 @@ package com.example.customviewstraining
 
 import android.content.Context
 import android.content.res.TypedArray
+import android.graphics.drawable.Drawable
 import android.util.AttributeSet
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.widget.Button
 import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.core.content.ContextCompat
 
 
-class CustomView(context: Context, attrs: AttributeSet) : ConstraintLayout(context, attrs) {
+class CustomView : ConstraintLayout {
+
+    constructor(context: Context) : super(context) {
+        initializeViews(context)
+    }
+
+    constructor(context: Context, attrs: AttributeSet) : super(context, attrs) {
+        initializeViews(context, attrs)
+    }
+
+    constructor(context: Context, attrs: AttributeSet, defStyleAttr: Int) : super(
+        context,
+        attrs,
+        defStyleAttr
+    ) {
+        initializeViews(context, attrs, defStyleAttr)
+    }
+
+    private val defButtonBackGreen: Drawable? =
+        ContextCompat.getDrawable(context, R.drawable.round_button_green)
+    private val defButtonBackRed: Drawable? =
+        ContextCompat.getDrawable(context, R.drawable.round_button_red)
+    private val defButtonBackYellow: Drawable? =
+        ContextCompat.getDrawable(context, R.drawable.round_button_yellow)
+
     private var numOfButtons: Int = 3
     private val TAG = "CustomView"
-    var mainButton: Button
-    var button1: Button
-    var button2: Button
-    var button3: Button
-    var button4: Button
-    var button5: Button
+    lateinit var mainButton: Button
+    lateinit var button1: Button
+    lateinit var button2: Button
+    lateinit var button3: Button
+    lateinit var button4: Button
+    lateinit var button5: Button
+    var button1BackgroundRes: Drawable? = defButtonBackGreen
+    var button2BackgroundRes: Drawable? = defButtonBackGreen
+    var button3BackgroundRes: Drawable? = defButtonBackRed
+    var button4BackgroundRes: Drawable? = defButtonBackYellow
+    var button5BackgroundRes: Drawable? = defButtonBackYellow
     var activeButtonsList = ArrayList<Button>()
     private var areButtonsActive = false
 
-    init {
-        initializeViews(context)
+
+    private fun initializeViews(
+        context: Context,
+        attrs: AttributeSet? = null,
+        defStyleAttr: Int? = null
+    ) {
+
         val typedArray: TypedArray = context.obtainStyledAttributes(attrs, R.styleable.CustomView)
         numOfButtons = typedArray.getInt(R.styleable.CustomView_numberOfSmallButtons, 3)
+        button1BackgroundRes =
+            typedArray.getDrawable(R.styleable.CustomView_button1Background)
+        button2BackgroundRes =
+            typedArray.getDrawable(R.styleable.CustomView_button2Background)
+        button3BackgroundRes =
+            typedArray.getDrawable(R.styleable.CustomView_button3Background)
+        button4BackgroundRes =
+            typedArray.getDrawable(R.styleable.CustomView_button4Background)
+        button5BackgroundRes =
+            typedArray.getDrawable(R.styleable.CustomView_button5Background)
         typedArray.recycle()
 
-        mainButton = this.findViewById<Button>(R.id.main_button)
-        button1 = this.findViewById<Button>(R.id.button1)
-        button2 = this.findViewById<Button>(R.id.button2)
-        button3 = this.findViewById<Button>(R.id.button3)
-        button4 = this.findViewById<Button>(R.id.button4)
-        button5 = this.findViewById<Button>(R.id.button5)
+        val inflater = context
+            .getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+        inflater.inflate(R.layout.custom_view, this)
+
+        mainButton = this.findViewById(R.id.main_button)
+        button1 = this.findViewById(R.id.button1)
+        button2 = this.findViewById(R.id.button2)
+        button3 = this.findViewById(R.id.button3)
+        button4 = this.findViewById(R.id.button4)
+        button5 = this.findViewById(R.id.button5)
+
 
         when (numOfButtons) {
             3 -> {
-                activeButtonsList.add(button1)
-                activeButtonsList.add(button3)
-                activeButtonsList.add(button5)
+                with(activeButtonsList) {
+                    button1.background = button1BackgroundRes
+                    add(button1)
+                    button3.background = button3BackgroundRes
+                    add(button3)
+                    button5.background = button5BackgroundRes
+                    add(button5)
+                }
             }
             4 -> {
-                activeButtonsList.add(button1)
-                activeButtonsList.add(button2)
-                activeButtonsList.add(button4)
-                activeButtonsList.add(button5)
+                with(activeButtonsList) {
+                    button1.background = button1BackgroundRes
+                    add(button1)
+                    button2.background = button2BackgroundRes
+                    add(button2)
+                    button4.background = button4BackgroundRes
+                    add(button4)
+                    button5.background = button5BackgroundRes
+                    add(button5)
+                }
             }
             5 -> {
-                activeButtonsList.add(button1)
-                activeButtonsList.add(button2)
-                activeButtonsList.add(button3)
-                activeButtonsList.add(button4)
-                activeButtonsList.add(button5)
+                with(activeButtonsList) {
+                    button1.background = button1BackgroundRes
+                    add(button1)
+                    button2.background = button2BackgroundRes
+                    add(button2)
+                    button3.background = button3BackgroundRes
+                    add(button3)
+                    button4.background = button4BackgroundRes
+                    add(button4)
+                    button5.background = button5BackgroundRes
+                    add(button5)
+                }
             }
         }
     }
 
-
-    private fun initializeViews(context: Context) {
-        val inflater = context
-            .getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
-        inflater.inflate(R.layout.custom_view, this)
-    }
-
     override fun onFinishInflate() {
         super.onFinishInflate()
-        mainButton.setOnLongClickListener(View.OnLongClickListener {
+        mainButton.setOnLongClickListener(OnLongClickListener {
             if (!areButtonsActive) {
                 Log.d(TAG, "longclick: ")
                 var latency = 0L
                 for (button in activeButtonsList) {
+                    button.setOnClickListener { nextColor(button) }
                     latency += 200L
                     button.postDelayed({
                         button.visibility = View.VISIBLE
@@ -92,5 +155,22 @@ class CustomView(context: Context, attrs: AttributeSet) : ConstraintLayout(conte
         })
 
 
+    }
+
+    private fun nextColor(button: Button) {
+        Log.d(TAG, "nextColor: called")
+        when (button.background) {
+            ContextCompat.getDrawable(context, R.drawable.round_button_red) -> {
+                button.background =
+                    ContextCompat.getDrawable(context, R.drawable.round_button_yellow)
+            }
+            ContextCompat.getDrawable(context, R.drawable.round_button_yellow) -> {
+                button.background =
+                    ContextCompat.getDrawable(context, R.drawable.round_button_green)
+            }
+            ContextCompat.getDrawable(context, R.drawable.round_button_green) -> {
+                button.background = ContextCompat.getDrawable(context, R.drawable.round_button_red)
+            }
+        }
     }
 }
--- app/src/main/java/com/example/customviewstraining/MainActivity.kt
+++ app/src/main/java/com/example/customviewstraining/MainActivity.kt
@@ -14,60 +14,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        /*main_button.setOnLongClickListener(View.OnLongClickListener {
-            Log.d(TAG, "longclick: ")
-            button.postDelayed({
-                Log.d(TAG, "onCreate: postdelayed called")
-                button.visibility = View.VISIBLE
-            }, 200)
-            button2.postDelayed({
-                button2.visibility = View.VISIBLE
-            }, 400)
-            button3.postDelayed({
-                button3.visibility = View.VISIBLE
-            }, 600)
-            return@OnLongClickListener true
-        })
-
-        main_button.setOnClickListener {
-            button.postDelayed({
-                Log.d(TAG, "onCreate: postdelayed called")
-                button.visibility = View.INVISIBLE
-            }, 600)
-            button2.postDelayed({
-                button2.visibility = View.INVISIBLE
-            }, 400)
-            button3.postDelayed({
-                button3.visibility = View.INVISIBLE
-            }, 200)
-        }
-
-        button.setOnClickListener {
-           nextColor(it)
-        }
-        button2.setOnClickListener {
-            nextColor(it)
-        }
-        button3.setOnClickListener {
-            nextColor(it)
-        }*/
-
-
-
-    }
-
-    private fun nextColor(button: View){
-        when(button.background){
-            ContextCompat.getDrawable(this, R.drawable.round_button_red) -> {
-                button.background = ContextCompat.getDrawable(this, R.drawable.round_button_yellow)
-            }
-            ContextCompat.getDrawable(this, R.drawable.round_button_yellow) -> {
-                button.background = ContextCompat.getDrawable(this, R.drawable.round_button_green)
-            }
-            ContextCompat.getDrawable(this, R.drawable.round_button_green) -> {
-                button.background = ContextCompat.getDrawable(this, R.drawable.round_button_red)
-            }
-        }
     }
 
 }
